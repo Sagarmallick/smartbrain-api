40b66139d52664b98233c650a8a594927a40144d
sagar mallick <sagarmallick711@gmail.com>
2021-01-10 23:38:16 -0800

    adding server files

---
+++ controller/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '610eb598cb2b456b97977151a59e8d18'
+   });
+
+   const handleApicall =(req, res)=>{
+       app.models
+       .predict(Clarifai.FACE_DETECT_MODEL,req.body.iput)
+       .then(data =>{
+           res.json(data);
+       })
+       .catch(err=> res.status(400).json('unable to work with API'))
+   }
+
+const handleImage=(req,res,db)=>{
+    const { id }= req.body;
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries=>{
+        res.json(entries[0]);
+    })
+    .catch(err=> res.status(400).json('unable to get entries'))
+}
+module.exports={
+    handleImage,
+    handleApicall
+}
+
+
